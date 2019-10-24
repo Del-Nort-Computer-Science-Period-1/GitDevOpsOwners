6d80307eb7e206561c8a957e5ece19f87a321667
Colin Szeto <kolinszeto@gmail.com>
2019-10-24 08:20:36 -0700

    Testing the multi file upload onto GitHub
    Testing the multi file upload onto GitHub

---
+++ ObjC/ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by Szeto, Colin on 10/23/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
